56110618e0359dd4dfe44494b2f7736f84215395
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-19 20:17:29 +0200

    Ruta Get por id implamentada junto a su respectivo controlador

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -3,7 +3,18 @@ from controllers import book_controllers
 from models.book_models import Book
 router = APIRouter()
 
+
+# Obtener todos los libros http://localhost:8000/books
 @router.get("/", status_code=200)
 async def get_books():
     return await book_controllers.get_all_books()
+
+# Obtener libror por id http://localhost:8000/books/{id}
+@router.get("/{id}", status_code=200)
+async def get_book_by_id(id: int):
+    return await book_controllers.get_a_book_by_id(id)
+
+
+
+
 
